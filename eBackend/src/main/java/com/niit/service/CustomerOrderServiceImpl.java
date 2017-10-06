b89552d7bf4829776cb1f1ae2c53f7f787301145
Vinitha15 <vinithachandrasekaran@gmail.com>
2017-10-06 15:55:26 +0530

    Add files via upload

--- eBackend/src/main/java/com/niit/service/CustomerOrderServiceImpl.java
+++ eBackend/src/main/java/com/niit/service/CustomerOrderServiceImpl.java
@@ -1,5 +1,7 @@
 package com.niit.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.niit.dao.CustomerOrderDao;
 import com.niit.model.Cart;
 import com.niit.model.CustomerOrder;
+import com.niit.model.ShippingAddress;
 
 @Service
 @Transactional
@@ -15,8 +18,22 @@ public class CustomerOrderServiceImpl implements CustomerOrderService {
 	@Autowired
 	private CustomerOrderDao customerorderdao;
 
-	public CustomerOrder Createorder(Cart cart) {
-		return customerorderdao.Createorder(cart);
+	public CustomerOrder Createorder(Cart cart, ShippingAddress s) {
+		return customerorderdao.Createorder(cart,s);
+	}
+
+	public List<ShippingAddress> getshippingbyid(int id) {
+		
+		return customerorderdao.getshippingbyid(id);
+	}
+
+	public ShippingAddress getshipping(int id) {
+		return customerorderdao.getshipping(id);
+	}
+
+	public void saveshipping(ShippingAddress shipping) {
+		customerorderdao.saveshipping(shipping);
+		
 	}
 
 }
